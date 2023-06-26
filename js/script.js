d5d69da337a2b20e602356650e8cab17b8978376
natamaz <sidorova87@mail.ru>
2023-06-26 10:56:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,120 @@
+'use strict';
+
+// professional-container
+$('.professional-container').slick({
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  speed: 300,
+  arrows:false,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
+});
+
+// brands-slider-1
+$('.brands-slider-1').slick({
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  speed: 300,
+  arrows:false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
+});
+
+// brands-slider-2
+$('.brands-slider-2').slick({
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  speed: 300,
+  arrows:false,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
+});
+
+// smart-slider
+$('.smart-slider').slick({
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  speed: 300,
+  arrows:false,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
+});
